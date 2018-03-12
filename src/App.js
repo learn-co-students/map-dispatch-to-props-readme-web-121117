db00f8421ed6017f5ede2f56a332efce14347e49
markbello <mcbello51286@gmail.com>
2018-03-12 15:52:55 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'; /* code change */
 import { addItem } from  './actions/items';
 
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    this.props.addItem();
   }
 
   render() {
@@ -27,4 +28,11 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+/* code change */
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    addItem: addItem
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
